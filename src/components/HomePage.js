e4c3fd46d562eddf22f821526c265c27cdba59d9
emirkugic <emir.kugic@hotmail.com>
2024-08-31 01:10:23 +0200

    FEATURE: AI chatbot implementation

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -44,6 +44,27 @@ const HomePage = () => {
 		};
 	}, [modalRef]);
 
+	// Inject Botpress chat bubble script
+	useEffect(() => {
+		const script1 = document.createElement("script");
+		script1.src = "https://cdn.botpress.cloud/webchat/v2.1/inject.js";
+		script1.async = true;
+
+		const script2 = document.createElement("script");
+		script2.src =
+			"https://mediafiles.botpress.cloud/825e8262-4f4d-4039-ac80-aedd4c88caae/webchat/v2.1/config.js";
+		script2.async = true;
+
+		document.body.appendChild(script1);
+		document.body.appendChild(script2);
+
+		// Clean up the scripts when the component is unmounted
+		return () => {
+			document.body.removeChild(script1);
+			document.body.removeChild(script2);
+		};
+	}, []);
+
 	const handlePlusClick = () => {
 		setShowModal(true);
 		setModalStage("initial");
